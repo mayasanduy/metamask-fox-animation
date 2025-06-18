a77479f9d5c3c85855883ab99544aff1cfd9b52a
mayasanduy <yaprezidentmoldavii@gmail.com>
2025-06-18 14:13:18 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AnimatedFoxWeb from './AnimatedFoxWeb';
+import AnimatedFoxTracked from './AnimatedFoxTracked';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AnimatedFoxWeb />);
+root.render(<AnimatedFoxTracked />);
