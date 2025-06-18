eabc532622af55800892f3859889983668d84dd5
mayasanduy <yaprezidentmoldavii@gmail.com>
2025-06-18 14:06:47 +0300

    Update foxHeadPaths.js

--- src/foxHeadPaths.js
+++ src/foxHeadPaths.js
@@ -1,12 +1,27 @@
 export const foxHeadPaths = [
   {
-    id: 'mock-ear-left',
-    d: 'M10,20 L30,5 L50,20 Z',
+    id: 'ear-left',
+    d: 'M100 40 L140 10 L160 50 Z',
+    fill: '#8C1A00',
+  },
+  {
+    id: 'ear-right',
+    d: 'M200 40 L160 10 L140 50 Z',
+    fill: '#8C1A00',
+  },
+  {
+    id: 'face-upper',
+    d: 'M140 50 L160 50 L170 100 L130 100 Z',
     fill: '#661800',
   },
   {
-    id: 'mock-ear-right',
-    d: 'M250,20 L270,5 L290,20 Z',
-    fill: '#661800',
+    id: 'face-lower',
+    d: 'M130 100 L170 100 L160 140 L140 140 Z',
+    fill: '#4B0D00',
   },
+  {
+    id: 'nose',
+    d: 'M145 135 L155 135 L150 145 Z',
+    fill: '#1C0000',
+  }
 ];
