9b71a445dc3bd505a47520fea0a1a593f8322804
mayasanduy <yaprezidentmoldavii@gmail.com>
2025-06-18 13:59:32 +0300

    Create foxHeadPaths.js

---
+++ src/foxHeadPaths.js
@@ -0,0 +1,12 @@
+export const foxHeadPaths = [
+  {
+    id: 'mock-ear-left',
+    d: 'M10,20 L30,5 L50,20 Z',
+    fill: '#661800',
+  },
+  {
+    id: 'mock-ear-right',
+    d: 'M250,20 L270,5 L290,20 Z',
+    fill: '#661800',
+  },
+];
