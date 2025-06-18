85d531bb2cb2a84b1cc4c8c299239fb6cc649269
mayasanduy <yaprezidentmoldavii@gmail.com>
2025-06-18 14:11:07 +0300

    Update foxHeadPaths.js

--- src/foxHeadPaths.js
+++ src/foxHeadPaths.js
@@ -1,27 +1,13 @@
 export const foxHeadPaths = [
-  {
-    id: 'ear-left',
-    d: 'M100 40 L140 10 L160 50 Z',
-    fill: '#8C1A00',
-  },
-  {
-    id: 'ear-right',
-    d: 'M200 40 L160 10 L140 50 Z',
-    fill: '#8C1A00',
-  },
-  {
-    id: 'face-upper',
-    d: 'M140 50 L160 50 L170 100 L130 100 Z',
-    fill: '#661800',
-  },
-  {
-    id: 'face-lower',
-    d: 'M130 100 L170 100 L160 140 L140 140 Z',
-    fill: '#4B0D00',
-  },
-  {
-    id: 'nose',
-    d: 'M145 135 L155 135 L150 145 Z',
-    fill: '#1C0000',
-  }
+  { id: 'left-ear', d: 'M60,30 L90,10 L110,50 Z', fill: '#5E2900' },
+  { id: 'right-ear', d: 'M240,30 L210,10 L190,50 Z', fill: '#5E2900' },
+  { id: 'forehead', d: 'M110,50 L150,20 L190,50 L150,110 Z', fill: '#F47C2C' },
+  { id: 'eye-left', d: 'M110,110 L130,100 L125,130 Z', fill: '#5E2900' },
+  { id: 'eye-right', d: 'M190,110 L170,100 L175,130 Z', fill: '#5E2900' },
+  { id: 'nose', d: 'M140,150 L160,150 L150,170 Z', fill: '#4A1F0F' },
+  { id: 'cheek-left', d: 'M60,140 L110,130 L110,170 L70,180 Z', fill: '#F18A50' },
+  { id: 'cheek-right', d: 'M240,140 L190,130 L190,170 L230,180 Z', fill: '#F18A50' },
+  { id: 'lower-left', d: 'M110,170 L150,210 L90,220 Z', fill: '#E15A0E' },
+  { id: 'lower-right', d: 'M190,170 L150,210 L210,220 Z', fill: '#E15A0E' },
+  { id: 'chin', d: 'M130,220 L150,230 L170,220 L150,210 Z', fill: '#D1D3D4' },
 ];
